8f84158830da5ec8f321e63c8a7bdf92c8d42c65
Denis <95048755+rowhe@users.noreply.github.com>
2022-09-09 19:30:07 +0300

    added sayAnother method and test for sayAnother method

--- src/main/java/plaindoll/HelloPlayer.java
+++ src/main/java/plaindoll/HelloPlayer.java
@@ -5,6 +5,7 @@ public class HelloPlayer{
 		Welcomer welcomer = new Welcomer();
 		System.out.println(welcomer.sayWelcome());
 		System.out.println(welcomer.sayFarewell());
+		System.out.println(welcomer.sayAnother());
 
 	}
 
